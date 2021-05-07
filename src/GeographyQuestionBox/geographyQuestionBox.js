a3f3ca51601e7b1351bc862c55c00be9b87d5674
Kavish Gandhi <kavishg@live.unc.edu>
2021-05-07 15:25:05 -0400

    made all categories and implemented APIs

--- src/GeographyQuestionBox/geographyQuestionBox.js
+++ src/GeographyQuestionBox/geographyQuestionBox.js
@@ -1,9 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import axios from 'axios/dist/axios';
 import { useEffect, useState } from 'react';
+import './geographyQuestionBox.css';
 
-function GeographyQuestionBox() {
+let answers = {};
 
-  const [question, setQuestion] = useState('');
+function GkQuestionBox() {
+  const [questionObjects, setQuestionObjects] = useState([]);
 
   useEffect( () => {
     const fetchData = async () => {
@@ -11,57 +15,93 @@ function GeographyQuestionBox() {
         url: 'https://opentdb.com/api.php?amount=10&category=22&difficulty=easy',
         method: 'get',
       });
-      let questionFix = result.data.results[0].question;
-      if (questionFix.search("&quot;") !== -1 || questionFix.search("&#039;") !== -1) {
-        questionFix = result.data.results[0].question.replace(/&quot;/g, "\"");
-        questionFix = result.data.results[0].question.replace(/&#039;/g, "\'");
+
+      let questionObjectsTemp = [];
+      for (let i = 0; i<result.data.results.length; i++) {
+        questionObjectsTemp.push(result.data.results[i]);
+        answers[""+i] = result.data.results[i].correct_answer;
       }
-      console.log(result);
-      setQuestion(questionFix);
+      setQuestionObjects(questionObjectsTemp);
     }
     fetchData();
   }, []);
 
+  console.log(questionObjects);
   return (
     <div class="columns is-centered">
-      <div class="card column is-half">
-        <header class="card-header">
-          <p class="card-header-title is-centered">
-            {question}
-          </p>
-        </header>
-        <div class="card-image">
-          <figure class="image is-16by9">
-            <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder Image"></img>
-          </figure>
+      <div class="column is-half">
+          <form class="box">
+          <h1 class="title is-1 has-text-centered">Welcome to General Knowledge Trivia!</h1>
+            {questionObjects.map((questionObj, index) => {
+              let options = [...questionObj.incorrect_answers].concat(questionObj.correct_answer);
+              shuffle(options);
+              return (
+              <div class="field" key={'question-'+index}>
+                <label class="label break_line">{decodeHtml(questionObj.question)}</label>
+                <div class="control" key={'options-'+index}>
+                  {options.map((option, optionIndex) => (
+                    <div class="control" key={'option-'+optionIndex}>
+                      <label class="label break_line">
+                        <input type="radio" name={"option"+index} class="radioButton" objindex={''+index} option={''+option}/>
+                          {"  "+decodeHtml(option)}
+                      </label>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )})}
+        </form>
+        <div class="column is-offset-10">
+          <button class="button is-black" onClick={handleSubmitButtonPress}>Submit</button>
         </div>
-
-        <div class="card-content">
-          <div class="content">
-            content
-          </div>
-        </div>
-
-        <footer class="card-footer">
-            <button class="button card-footer-item">
-              <span class="icon-text">
-                <span class="icon">
-                    <i class="fas fa-arrow-left"></i>
-                </span>
-                <span>Previous</span>
-              </span>
-            </button>
-          <button class="button card-footer-item">
-            <span class="icon-text">
-              <span>Next</span>
-                <span class="icon">
-                  <i class="fas fa-arrow-right"></i>
-                </span>
-            </span>
-          </button>
-        </footer>
       </div>
     </div>
   );
 }
-export default GeographyQuestionBox;
+
+function decodeHtml(html) {
+  let txt = document.createElement("textarea");
+  txt.innerHTML = html;
+  return txt.value;
+}
+
+function shuffle(array) {
+  for(let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * i);
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}
+
+function handleSubmitButtonPress(event) {
+  let radioButtons = [];
+  let answersEntered = {};
+  radioButtons = document.getElementsByClassName('radioButton');
+  for (let i = 0; i < radioButtons.length; i++) {
+    if (radioButtons[i].checked) {
+      let objIndex = radioButtons[i].getAttribute("objindex");
+      answersEntered[''+objIndex] = radioButtons[i].getAttribute('option');
+    }
+  }
+  let keys1 = Object.keys(answersEntered);
+  let keys2 = Object.keys(answers);
+  let incorrectAnswers = {};
+  if(keys1.length === keys2.length) {
+    for(let i = 0; i < keys1.length; i++) {
+      if (answers[""+i] !== answersEntered[""+i]) {
+        incorrectAnswers[""+i] = answersEntered[""+i];
+      }
+    }
+  }
+  console.log(incorrectAnswers);
+  let incorrectKeys = Object.keys(incorrectAnswers);
+  let printKeys = incorrectKeys.map(key => parseInt(key)+1);
+  if (incorrectKeys.length !== 0) {
+    alert("You lost! The following questions were wrong: "+printKeys);
+  } else {
+    alert("You won! Try out another category!");
+  }
+}
+
+export default GkQuestionBox;
--- src/TitlePane/titlePane.js
+++ src/TitlePane/titlePane.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import HomePage from './../HomePage/homePage';
+import GkQuestionBox from './../GkQuestionBox/gkQuestionBox';
+import AnimalQuestionBox from './../AnimalQuestionBox/animalQuestionBox';
+import MathQuestionBox from './../MathQuestionBox/mathQuestionBox';
+import FilmQuestionBox from './../FilmQuestionBox/filmQuestionBox';
+import GeographyQuestionBox from './../GeographyQuestionBox/geographyQuestionBox';
 
 function TitlePane() {
     return (
         <div class="columns has-text-centered">
-            <div class="column is-2">
+            <div class="column is-4">
                 <button class="button is-dark" id="home" onClick={homeButtonPress}>
                     <span class="icon-text">
                         <span class="icon">
@@ -15,42 +20,118 @@ function TitlePane() {
                     </span>
                 </button>
             </div>
-            <div class="box column columns is-offset-6">
+            <div class="box columns ">
                 <div class="column">
                     <span class="icon-text">
-                        <span>Difficulty Level</span>
+                        <span>Categories</span>
                         <span class="icon">
                             <i class="fas fa-arrow-right"></i>
                         </span>
                     </span>
                 </div>
                 <div class="column">
-                    <button class="button is-dark">
-                        Easy
-                    </button>
+                    <div class="columns">
+                        <div class="column">
+                            <button class="button is-dark" id="gk" onClick={handleButtonPress}>
+                                General Knowledge
+                            </button>
+                        </div>
+                        <div class="column">
+                            <button class="button is-dark" id="animals" onClick={handleButtonPress}>
+                                Animals
+                            </button>
+                        </div>
+                        <div class="column">
+                            <button class="button is-dark" id="mathematics" onClick={handleButtonPress}>
+                                Mathematics
+                            </button>
+                        </div>
+                        <div class="column">
+                            <button class="button is-dark" id="film" onClick={handleButtonPress}>
+                                Film
+                            </button>
+                        </div>
+                        <div class="column">
+                            <button class="button is-dark" id="geography" onClick={handleButtonPress}>
+                                Geography
+                            </button>
+                        </div>
+                    </div>
                 </div>
-                <div class="column">
-                    <button class="button is-dark">
-                        Medium
-                    </button>
-                </div>
-                <div class="column">
-                    <button class="button is-dark">
-                        Hard
-                    </button>
-                </div>
+                            
             </div>
         </div>
     );
 }
 
 function homeButtonPress(event) {
+    window.location.reload();
     ReactDOM.render(
         <React.StrictMode>
           <HomePage/> 
         </React.StrictMode>,
         document.getElementById('root')
       );
+}
+
+function handleButtonPress(event) {
+    let buttonType = event.target.getAttribute('id');
+    if(buttonType === "gk") {
+        ReactDOM.render(
+            <React.StrictMode>
+                <br></br>
+                <TitlePane />
+                <br></br>
+                <GkQuestionBox />
+                <br></br>
+            </React.StrictMode>,
+            document.getElementById('root')
+        );
+    } else if (buttonType === "animals") {
+        ReactDOM.render(
+            <React.StrictMode>
+                <br></br>
+                <TitlePane />
+                <br></br>
+                <AnimalQuestionBox />
+                <br></br>
+            </React.StrictMode>,
+            document.getElementById('root')
+        );
+    } else if (buttonType === "mathematics") {
+        ReactDOM.render(
+            <React.StrictMode>
+                <br></br>
+                <TitlePane />
+                <br></br>
+                <MathQuestionBox />
+                <br></br>
+            </React.StrictMode>,
+            document.getElementById('root')
+        );
+    } else if (buttonType === "film") {
+        ReactDOM.render(
+            <React.StrictMode>
+                <br></br>
+                <TitlePane />
+                <br></br>
+                <FilmQuestionBox />
+                <br></br>
+            </React.StrictMode>,
+            document.getElementById('root')
+        );
+    } else if (buttonType === "geography") {
+        ReactDOM.render(
+            <React.StrictMode>
+                <br></br>
+                <TitlePane />
+                <br></br>
+                <GeographyQuestionBox />
+                <br></br>
+            </React.StrictMode>,
+            document.getElementById('root')
+        );
+    }
 }
 
 export default TitlePane;
